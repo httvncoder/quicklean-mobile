dfa0549005231c98c3ab563267fad0f35279120c
srph <seraphipod@gmail.com>
2016-09-09 17:13:52 +0000

    Queue: Add step indicator

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,5 +1,22 @@
 angular.module('app.directives', [])
 
-.directive('blankDirective', [function(){
+.directive('wizardSteps', [function() {
+	return {
+		scope: {
+			step: '=',
+			max: '='
+		},
 
+		template: [
+			'<div class="bar bar-footer bar-stable">',
+				'<div class="wizard-steps u-block-center">',
+					'<div ng-repeat-start="current in [1,2,3]" ng-class="{\'wizard-step\': true, \'wizard-step--inactive\': current > step}">',
+						'<i ng-if="current <= step" class="icon ion-checkmark"></i>',
+					'</div>',
+
+					'<div ng-repeat-end ng-class="{\'wizard-line\': true, \'wizard-line--hidden\': current === max}"></div>',
+				'</div>',
+			'</div>'
+		].join('\n')
+	};
 }]);
