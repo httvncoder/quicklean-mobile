ed40e52704c251f0f54e1c7b1795c71ee6586f1a
srph <seraphipod@gmail.com>
2016-09-10 22:29:38 +0800

    Queue: Display machine / queue info

--- www/js/app.js
+++ www/js/app.js
@@ -5,8 +5,14 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.directives','app.services',])
-
+angular.module('app', [
+  'ionic',
+  'app.controllers'
+  'app.routes',
+  'app.directives',
+  'app.services',
+  'ordinal'
+])
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
