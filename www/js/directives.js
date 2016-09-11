22c18a85b012c24055b5163b55bcc1a547ea84bc
srph <seraphipod@gmail.com>
2016-09-12 06:39:02 +0800

    Rename and refactor `wizard-steps` stylesheet

--- www/js/directives.js
+++ www/js/directives.js
@@ -8,12 +8,12 @@ angular.module('app.directives', [])
 		},
 
 		template: [
-			'<div class="wizard-steps">',
-				'<div ng-repeat-start="current in [1,2,3]" ng-class="{\'wizard-step\': true, \'wizard-step--inactive\': current > step}">',
+			'<div class="wizard">',
+				'<div ng-repeat-start="current in [1,2,3]" ng-class="{\'wizard__step\': true, \'wizard__step--inactive\': current > step}">',
 					'<i ng-if="current <= step" class="icon ion-checkmark"></i>',
 				'</div>',
 
-				'<div ng-repeat-end ng-class="{\'wizard-line\': true, \'wizard-line--hidden\': current === max}"></div>',
+				'<div ng-repeat-end ng-if="current !== max" class="wizard__line"></div>',
 			'</div>',
 		].join('\n')
 	};
